93c696bd1d83a6642013f8e06f17807db08902db
Antonioarf <antonio.awad.roxo@gmail.com>
2023-05-12 09:54:33 -0300

    novo exec

--- python/exec.py
+++ python/exec.py
@@ -1,15 +1,24 @@
 import os
 import subprocess
+from pathlib import Path
 
 
-entries = os.listdir('input/')
-algotimo = 'aleatoria_invertida'
-os.system(f'g++ -Wall -O3 {algotimo}.cpp -o {algotimo}')
-for inp in entries:
-    with open(f'input/{inp}') as f:
-        proc = subprocess.run(["./{}".format(algotimo)], input=f.read(), text=True, capture_output=True)
-        #print('Saída:', proc.stdout)
-
-    with open('output/{}/out_{}_{}.txt'.format(algotimo,algotimo,inp[6:-4]), 'w') as f:
-        f.write(proc.stdout)
-        
+pai = (Path(__file__).parent).parent
+entries = os.listdir(f"{pai}/input")
+cpps = f"{pai}/CPP/"
+# algos  = ["gulosa","gulosa_invertida","aleatoria","aleatoria_invertida","exaustiva_paralela","exaustiva"]
+algos = ["exaustiva_paralela"]
+for algo in algos:
+    if algo == "exaustiva" or algo == "exaustiva_paralela":
+        os.system(f'g++ -Wall -fopenmp -O3 {cpps}/{algo}.cpp -o {pai}/CPP/{algo}')
+    else:
+        os.system(f'g++ -Wall -O3 {cpps}/{algo}.cpp -o {pai}/CPP/{algo}')
+    
+    print(algo)
+    for inp in entries:
+        with open(f'{pai}/input/{inp}') as f:
+            proc = subprocess.run([f"./CPP/{algo}"], input=f.read(), text=True, capture_output=True)
+            #print('Saída:', proc.stdout)
+        with open('{}/output/{}/out_{}_{}.txt'.format(pai,algo,algo,inp[6:-4]), 'w') as f:
+            f.write(proc.stdout)
+    
